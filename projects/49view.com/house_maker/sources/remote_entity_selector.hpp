6457da8827ca300d23dd7824fb0ff25c3b3ee99f
DadoLinux <ziocleto@gmail.com>
2020-07-12 07:28:16 +0100

    mainly updated submodules

--- projects/49view.com/house_maker/sources/remote_entity_selector.hpp
+++ projects/49view.com/house_maker/sources/remote_entity_selector.hpp
@@ -5,7 +5,10 @@
 #pragma once
 
 #include <core/resources/resource_metadata.hpp>
+#include <core/util.h>
 #include <graphics/imgui/imgui.h>
+#include <poly/scene_graph.h>
+#include <render_scene_graph/render_orchestrator.h>
 
 #include <eh_arch/models/house_service.hpp>
 #include <eh_arch/render/house_render.hpp>
@@ -95,7 +98,7 @@ public:
                         auto im = rsg.TH( meta.thumb.empty() ? S::WHITE : meta.thumb);
                         if ( im ) {
                             if ( ImGui::ImageButton(ImGuiRenderTexture(im), ImVec2(thumbSize, thumbSize)) ) {
-                                backEnd->process_event(OnAddFurnitureSingleEvent{_resource, meta.hash});
+                                backEnd->process_event(OnAddFurnitureSingleEvent{_resource, FurnitureSet{FT::FT_Sofa, meta.hash, meta.bboxSize, S::SQUARE}});
                             }
                             auto sanitizedTags = tagsSanitisedFor(query, meta.group, meta.tags);
                             if ( ImGui::IsItemHovered() ) {
