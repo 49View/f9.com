6629e9979821a1afb28525ab4aeff2d65c7ba99a
DadoLinux <ziocleto@gmail.com>
2020-07-16 16:14:43 +0100

    Pagination moved to future modules

--- projects/49view.com/src/components/excalibur/EntityBrowser.js
+++ projects/49view.com/src/components/excalibur/EntityBrowser.js
@@ -1,66 +1,16 @@
 import {
   ButtonDiv,
-  Div,
   DivBorder,
   Flex,
   Img100,
-  Logo1TextSpan,
-  Mx05,
   My05,
   My075,
-  My25,
-  SecondaryAltColorTextSpanBold
+  My25
 } from "../../futuremodules/reactComponentStyles/reactCommon.styled";
-import {Badge, FormControl, InputGroup, Pagination, Tab, Tabs} from "react-bootstrap";
+import {Badge, FormControl, InputGroup, Tab, Tabs} from "react-bootstrap";
 import React, {useState} from "react";
 import {useQLEntityMeta} from "./ExcaliburLogic";
-
-export const PaginationResultHeader = ({searchQuery, pageInfo, setPage}) => {
-  const bNoPages = pageInfo.lastPage === 0;
-  const bSinglePage = pageInfo.lastPage !== 0 && pageInfo.lastPage < 3;
-  const bComplexPages = pageInfo.lastPage >= 3;
-  let complexPageIndex1 = 0;
-  let complexPageIndex2 = 0;
-  let complexPageIndex3 = 0;
-  if ( bComplexPages ) {
-    const isFirstPage = pageInfo.page === 0;
-    const isLastPage = pageInfo.page === pageInfo.lastPage;
-    const lastPageOff = isLastPage ? -2 : -1;
-    complexPageIndex1 = isFirstPage ? 1 : pageInfo.page + lastPageOff +1;
-    complexPageIndex2 = complexPageIndex1 + 1;
-    complexPageIndex3 = complexPageIndex2 + 1;
-  }
-
-  return <Flex width={"100%"} background={"var(--dark-color-transparent-text-readable)"}
-               padding={bNoPages ? "8px" : "4px"}
-               justifyContent={"center"} borderRadius={"4px"}>
-    <Div>
-      <SecondaryAltColorTextSpanBold>{pageInfo.totalCount}{" "}</SecondaryAltColorTextSpanBold>
-      <Logo1TextSpan> {searchQuery}</Logo1TextSpan>
-      <SecondaryAltColorTextSpanBold> available</SecondaryAltColorTextSpanBold>
-    </Div>
-    <Mx05/>
-    <Pagination size={"sm"} className={"mb-0"} variant={"info"}>
-      {bSinglePage &&
-      Array.from(Array(pageInfo.lastPage + 1), (x, index) => index).map(elem =>
-        <Pagination.Item key={elem} active={elem===pageInfo.page} onClick={ () => setPage(elem)}>
-          {elem + 1}
-        </Pagination.Item>)}
-      {bComplexPages && <Pagination>
-        <Pagination.First disabled={0 === pageInfo.page} onClick={ () => setPage(0)} />
-        <Pagination.Prev disabled={!pageInfo.hasPreviousPage} onClick={ () => setPage(pageInfo.page-1)}/>
-
-        <Pagination.Item active={pageInfo.page === complexPageIndex1-1} onClick={ () => setPage(complexPageIndex1-1)}>{complexPageIndex1}</Pagination.Item>
-        <Pagination.Item active={pageInfo.page === complexPageIndex2-1} onClick={ () => setPage(complexPageIndex2-1)}>{complexPageIndex2}</Pagination.Item>
-        <Pagination.Item active={pageInfo.page === complexPageIndex3-1} onClick={ () => setPage(complexPageIndex3-1)}>{complexPageIndex3}</Pagination.Item>
-
-        <Pagination.Next disabled={!pageInfo.hasNextPage} onClick={ () => setPage(pageInfo.page+1)}/>
-        <Pagination.Last disabled={pageInfo.lastPage === pageInfo.page} onClick={ () => setPage(pageInfo.lastPage)}/>
-      </Pagination>
-      }
-    </Pagination>
-  </Flex>
-}
+import {PaginationResults} from "../../futuremodules/pagination/Pagination";
 
 export const EntityBrowser = ({width, dispatch, refreshToken}) => {
 
@@ -73,18 +23,20 @@ export const EntityBrowser = ({width, dispatch, refreshToken}) => {
   console.log(pageInfo);
 
   return <>
-    <Tabs defaultActiveKey={group} id="entity-browser-toolbar-tab" onSelect={k => {
-      setGroup(k);
-    }}>
-      <Tab eventKey="geom" title="Objects">
-      </Tab>
-      <Tab eventKey="material" title="Materials">
-      </Tab>
-      <Tab eventKey="image" title="Images">
-      </Tab>
-      <Tab eventKey="profile" title="Profiles">
-      </Tab>
-    </Tabs>
+    <Flex width={width}>
+      <Tabs defaultActiveKey={group} id="entity-browser-toolbar-tab" onSelect={k => {
+        setGroup(k);
+      }}>
+        <Tab eventKey="geom" title="Objects">
+        </Tab>
+        <Tab eventKey="material" title="Materials">
+        </Tab>
+        <Tab eventKey="image" title="Images">
+        </Tab>
+        <Tab eventKey="profile" title="Profiles">
+        </Tab>
+      </Tabs>
+    </Flex>
     <My05/>
     <Flex width={width}>
       <InputGroup>
@@ -100,12 +52,10 @@ export const EntityBrowser = ({width, dispatch, refreshToken}) => {
     <My075/>
 
     <Flex width={width}>
-      {pageInfo && pageInfo.totalCount > 0 &&
-      <PaginationResultHeader searchQuery={searchQuery} pageInfo={pageInfo} setPage={setPage}/>
-      }
+      <PaginationResults searchQuery={searchQuery} pageInfo={pageInfo} setPage={setPage}/>
     </Flex>
 
-    <Flex flexDirection={"column"} flexWrap={"wrap"} height={"500px"} margin={"0"}
+    <Flex flexDirection={"column"} flexWrap={"wrap"} height={"600px"} margin={"0"}
           alignItems={"flex-start"} alignContent={"flex-start"} justifyContent={"flex-start"}>
       {entities && entities.map(elem =>
         <DivBorder key={elem._id} variant={"info"} background={"var(--dark)"} padding={"4px"}
@@ -114,10 +64,8 @@ export const EntityBrowser = ({width, dispatch, refreshToken}) => {
           <ButtonDiv onClick={() => {
             window.Module.addScriptLine(`rr.addSceneObject("${elem.hash}", "${group}", false)`);
             dispatch(['loadEntity', elem]);
-          }
-          }>
-            <Img100
-              src={`https://${process.env.REACT_APP_EH_CLOUD_HOST}/media/entities/${elem.group}/${elem.thumb}`}></Img100>
+          }}>
+            <Img100 src={`https://${process.env.REACT_APP_EH_CLOUD_HOST}/media/entities/${elem.group}/${elem.thumb}`}/>
           </ButtonDiv>
           <My25/>
           {elem.tags.map(tag => <Badge key={tag} variant={"secondary"} className={"m-1"}>{tag}</Badge>)}
