3982cd8b1668dc0a41d08ad4f1ed5b2ba93522bd
DadoLinux <ziocleto@gmail.com>
2020-08-01 18:18:48 +0100

    Update stripeController.js

--- projects/49view.com-graphql-api/restful-api/controllers/stripeController.js
+++ projects/49view.com-graphql-api/restful-api/controllers/stripeController.js
@@ -8,7 +8,7 @@ exports.getSession = async skId => {
         name: "49view tier1",
         description: "Small business service premium",
         images: ["https://49view.com/ehlogo.svg"],
-        amount: 10000,
+        amount: 60000,
         currency: "gbp",
         quantity: 1
       }
