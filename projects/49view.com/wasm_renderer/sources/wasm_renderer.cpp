a988b4c5f8bb39a67c23b3ee69a68f27b1868581
DadoLinux <ziocleto@gmail.com>
2020-10-19 17:17:17 +0100

    updated submodules

--- projects/49view.com/wasm_renderer/sources/wasm_renderer.cpp
+++ projects/49view.com/wasm_renderer/sources/wasm_renderer.cpp
@@ -73,7 +73,7 @@ void Showcaser::activatePostLoad() {
     HOD::resolver<OSMData>(sg, nullptr, [&]() {
         rsg.useSkybox(true);
         OSMData map{FM::readLocalFileC("../elements.json")};
-        auto cc = sg.GB<GT::OSMTile>(&map, V2f{-0.1344f, 51.4892f}, GT::Tag(SHADOW_MAGIC_TAG), GT::Bucket(GTBucket::NearUnsorted), GT::M("city,atlas"), GT::Program(S::SH_CITY_ATLAS));
+        sg.GB<GT::OSMTile>(&map, V2f{-0.1344f, 51.4892f}, GT::Tag(SHADOW_MAGIC_TAG), GT::Bucket(GTBucket::NearUnsorted), GT::M("city,atlas"), GT::Program(S::SH_CITY_ATLAS));
         sg.GB<GT::OSMBuildings>(&map, V2f{-0.1344f, 51.4892f}, GT::Bucket(GTBucket::Near), GT::M("city,atlas"), GT::Program(S::SH_CITY_ATLAS));
     });
 
