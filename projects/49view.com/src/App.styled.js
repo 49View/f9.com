cae791cc34d4dea998bbeb04ccc0375c36704fba
DadoLinux <ziocleto@gmail.com>
2020-11-03 23:51:34 +0000

    Removing background image for now

--- projects/49view.com/src/App.styled.js
+++ projects/49view.com/src/App.styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Content = styled.section` {
-    background: url("/grant-UhpYKnqZwE8-unsplash.jpg") no-repeat center center
-      fixed;
+    //background: url("/grant-UhpYKnqZwE8-unsplash.jpg") no-repeat center center
+    //  fixed;
     height: auto;
     min-height: 100vh;  
     background-size: cover;
